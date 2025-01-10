c81a4cde2cfe2056659bf0e1abb02ed776b0f7bf
cz <me@czcs.xyz>
2025-01-10 00:27:14 -0500

    add basic form functionality

--- src/routes/(app)/bugreport/+page.svelte
+++ src/routes/(app)/bugreport/+page.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
+  import Button from '$lib/components/button/Primary.svelte';
   import Input from '$lib/components/form/Input.svelte';
   import Container from '$lib/components/pages/Container.svelte';
   import Title from '$lib/components/pages/Title.svelte';
 
   let description = $state('');
+  let submitting = $state(false);
+
+  let { form } = $props();
 </script>
 
 <Container>
   <Title extraProps="mb-2">Bug Report</Title>
-  <p class="text-sm text-zinc-400">
-    Thank you for taking the time to report a bug! Please fill out the form below, and we'll contact
-    you when we fix it.
-  </p>
+  <p class="text-sm text-zinc-400">this is for you ADRIAN</p>
   <div class="my-6"></div>
-  <div class="space-y-2">
+  <div class="space-y-2 *:w-full">
     <p class="text-sm font-medium">Bug description</p>
     <Input
       bind:value={description}
       placeholder="Where did you go? What happened to lead up to the bug? Explain in detail."
     />
   </div>
+  <Button form="form" disabled={submitting}>Submit</Button>
 </Container>
-<form id="form" method="POST">
+<form
+  id="form"
+  method="POST"
+  use:enhance={async () => {
+    submitting = true;
+    return ({ update }) => {
+      submitting = false;
+      update({ reset: false });
+    };
+  }}
+>
   <input hidden aria-hidden="true" name="description" bind:value={description} />
 </form>
