86750c321bcf7d3e8fd830b6e427a7e50cc41609
cz <me@czcs.xyz>
2025-01-12 14:29:45 -0500

    sidenav light/dark

--- src/lib/icons/Bug.svelte
+++ src/lib/icons/Bug.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  let { size = 4, fill = 'zinc-50' }: { size?: number; fill?: string } = $props();
+  let { size = 4 }: { size?: number } = $props();
 </script>
 
 <svg
-  class="size-4 stroke-{fill}"
+  class="size-4 stroke-zinc-950 dark:stroke-zinc-50"
   viewBox="0 0 24 24"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
