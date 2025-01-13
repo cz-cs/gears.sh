c1e29d492e44b1558c065efdde71214268e7d4ca
cz <me@czcs.xyz>
2025-01-12 19:39:40 -0500

    fixesssss

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,9 +1,8 @@
-import { Account, Avatars } from 'node-appwrite';
+import { Account } from 'node-appwrite';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
   const account = new Account(locals.client!);
-  const avatars = new Avatars(locals.client!);
 
   const session = await account.get();
 
