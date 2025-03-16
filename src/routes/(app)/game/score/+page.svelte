1d896de814c81a5196207c86586dc2c6a517190f
cz <me@czcs.xyz>
2025-03-16 18:52:49 -0400

    enhance game timer functionality

---
+++ src/routes/(app)/game/score/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Primary from '$lib/components/button/Primary.svelte';
+  import Secondary from '$lib/components/button/Outline.svelte';
+  import Container from '$lib/components/pages/Container.svelte';
+  import Title from '$lib/components/pages/Title.svelte';
+  import Input from '$lib/components/form/Input.svelte';
+
+  let total = $state(0);
+
+  let scored = $state('');
+</script>
+
+<svelte:head>
+  <title>Score calculator - gears</title>
+</svelte:head>
+
+<Container extraProps="flex flex-col min-h-screen">
+  <Title>Score calculator</Title>
+  <div class="m-auto lg:w-1/2">
+    <div class="rounded-md border border-zinc-200 p-6 shadow dark:border-zinc-800">
+      <div class="flex items-center gap-3">
+        <Input bind:value={scored} placeholder="0" />
+        <p>balls scored</p>
+      </div>
+      <div class="flex items-center gap-3">
+        <Input bind:value={scored} placeholder="0" />
+        <p>switches cleared</p>
+      </div>
+      <p class="text-center text-5xl font-medium lg:text-7xl">{total}</p>
+    </div>
+  </div>
+</Container>
--- src/routes/(app)/game/timer/+page.svelte
+++ src/routes/(app)/game/timer/+page.svelte
@@ -11,6 +11,7 @@
 
   import iqIcon from '$lib/icons/vex/iq.svg';
   import v5Icon from '$lib/icons/vex/v5.svg';
+  import { blur, fade, scale, slide } from 'svelte/transition';
 
   let countAudio: HTMLAudioElement;
   let startAudio: HTMLAudioElement;
@@ -21,8 +22,7 @@
   let status = $state('not started');
 
   let program = $state('iq');
-  let type = $state('teamwork');
-  let skill: string | undefined = undefined;
+  let type = $state(true);
 
   let countdown = $state(true);
   let sound = $state(false);
@@ -71,6 +71,29 @@
   }
 
   function timerStart() {
+    if (type == false) {
+      status = 'skills';
+      time = '1:00';
+
+      timeInterval = setInterval(async () => {
+        let seconds = toSec(time);
+        seconds--;
+
+        time = formatTime(seconds);
+
+        if (seconds == 0) {
+          stopAudio.play();
+
+          clearInterval(timeInterval);
+          timeInterval = undefined;
+
+          time = '0:00';
+          status = 'match ended';
+        }
+      }, 1000);
+      return;
+    }
+
     if (program != 'iq') {
       status = 'autonomous period';
       if (program == 'v5') time = '0:15';
@@ -187,68 +210,53 @@
 <audio bind:this={boomAudio} src={boomSfx} bind:muted={sound}></audio>
 
 <Container extraProps="flex flex-col min-h-screen">
-  <div class="flex w-full justify-between">
-    <Title>Game timer</Title>
-    <div class="flex gap-3">
-      <button
-        onclick={() => {
-          program = 'iq';
-        }}>iq</button
-      >
-      <button
-        onclick={() => {
-          program = 'v5';
-        }}>v5</button
-      >
-      <button
-        onclick={() => {
-          program = 'u';
-        }}>u</button
-      >
-    </div>
-    <div class="flex gap-3">
-      {#if program == 'iq'}
-        <button
-          onclick={() => {
-            skill = undefined;
-            type = 'teamwork';
-          }}>teamwork</button
-        >
-      {:else}
-        <button
-          onclick={() => {
-            skill = undefined;
-            type = 'teamwork';
-          }}>elimination</button
-        >
-      {/if}
-      <button
-        onclick={() => {
-          skill = undefined;
-          type = 'skills';
-        }}>skills</button
-      >
-    </div>
-    {#if type == 'skills'}
-      <div class="flex gap-3">
-        <button
-          onclick={() => {
-            skill = 'driver';
-          }}>driver</button
-        >
-        <button
-          onclick={() => {
-            program = 'autonomous';
-          }}>autonomous</button
-        >
-      </div>
-    {/if}
-  </div>
+  <Title>Game timer</Title>
   <div class="m-auto lg:w-1/2">
-    <div class="rounded-md border border-zinc-200 p-6 shadow dark:border-zinc-800">
-      <p class="text-center text-xs tracking-[0.27em] text-zinc-400 lowercase lg:text-sm">
-        {status}
-      </p>
+    <div class="rounded-xl border border-zinc-200 p-6 shadow dark:border-zinc-800">
+      <div class="flex w-full items-center justify-between">
+        <div class="inline-flex h-9 rounded-lg bg-zinc-800 p-1 text-zinc-400">
+          <button
+            data-active={program == 'iq'}
+            onclick={() => (program = 'iq')}
+            class="inline-flex cursor-pointer items-center justify-center rounded-md px-3 py-1 text-xs font-medium transition-colors focus-visible:ring-2 focus-visible:ring-zinc-50 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:outline-none data-[active=true]:bg-zinc-950 data-[active=true]:text-zinc-50"
+          >
+            IQ
+          </button>
+          <button
+            data-active={program == 'v5'}
+            onclick={() => (program = 'v5')}
+            class="inline-flex cursor-pointer items-center justify-center rounded-md px-3 py-1 text-xs font-medium transition-colors focus-visible:ring-2 focus-visible:ring-zinc-50 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:outline-none disabled:cursor-default disabled:opacity-50 data-[active=true]:bg-zinc-950 data-[active=true]:text-zinc-50"
+          >
+            V5
+          </button>
+          <button
+            data-active={program == 'u'}
+            onclick={() => (program = 'u')}
+            class="inline-flex cursor-pointer items-center justify-center rounded-md px-3 py-1 text-xs font-medium transition-colors focus-visible:ring-2 focus-visible:ring-zinc-50 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:outline-none disabled:cursor-default disabled:opacity-50 data-[active=true]:bg-zinc-950 data-[active=true]:text-zinc-50"
+          >
+            U
+          </button>
+        </div>
+        <p class="text-center text-xs tracking-[0.27em] text-zinc-400 lowercase lg:text-sm">
+          {status}
+        </p>
+        <div class="inline-flex h-9 rounded-lg bg-zinc-800 p-1 text-zinc-400">
+          <button
+            data-active={type == true}
+            onclick={() => (type = true)}
+            class="inline-flex cursor-pointer items-center justify-center rounded-md px-3 py-1 text-xs font-medium transition-colors focus-visible:ring-2 focus-visible:ring-zinc-50 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:outline-none disabled:cursor-default disabled:opacity-50 data-[active=true]:bg-zinc-950 data-[active=true]:text-zinc-50"
+          >
+            Regular
+          </button>
+          <button
+            data-active={type == false}
+            onclick={() => (type = false)}
+            class="inline-flex cursor-pointer items-center justify-center rounded-md px-3 py-1 text-xs font-medium transition-colors focus-visible:ring-2 focus-visible:ring-zinc-50 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:outline-none data-[active=true]:bg-zinc-950 data-[active=true]:text-zinc-50"
+          >
+            Skills
+          </button>
+        </div>
+      </div>
       <p class="my-6 text-center text-8xl font-medium lg:text-9xl">{time}</p>
       <div class="flex items-center justify-between gap-3">
         <Primary onclick={start} disabled={timeInterval != undefined} extraProps="w-full"
@@ -257,27 +265,31 @@
           >Stop</Secondary
         >
       </div>
-      <div class="my-3 h-px w-full bg-zinc-200 dark:bg-zinc-800"></div>
-      <div class="space-y-1">
-        <div class="flex gap-2">
-          <button
-            aria-label="Countdown"
-            data-checked={countdown}
-            onclick={() => (countdown = !countdown)}
-            class="h-4 w-4 cursor-pointer rounded-sm border border-zinc-950 shadow transition-all data-[checked=false]:bg-zinc-50 data-[checked=true]:bg-zinc-950 dark:border-zinc-50 dark:data-[checked=false]:bg-zinc-950 dark:data-[checked=true]:bg-zinc-50"
-          ></button>
-          <p class="text-xs font-medium">3 second countdown</p>
+      {#if status == 'not started' || status == 'match ended' || status == 'match ended abruptly'}
+        <div transition:slide={{ duration: 300 }}>
+          <div class="my-3 h-px w-full bg-zinc-200 dark:bg-zinc-800"></div>
+          <div class="space-y-1">
+            <div class="flex gap-2">
+              <button
+                aria-label="Countdown"
+                data-checked={countdown}
+                onclick={() => (countdown = !countdown)}
+                class="h-4 w-4 cursor-pointer rounded-sm border border-zinc-950 shadow transition-all data-[checked=false]:bg-zinc-50 data-[checked=true]:bg-zinc-950 dark:border-zinc-50 dark:data-[checked=false]:bg-zinc-950 dark:data-[checked=true]:bg-zinc-50"
+              ></button>
+              <p class="text-xs font-medium">3 second countdown</p>
+            </div>
+            <div class="flex gap-2">
+              <button
+                aria-label="Sound"
+                data-checked={!sound}
+                onclick={() => (sound = !sound)}
+                class="h-4 w-4 cursor-pointer rounded-sm border border-zinc-950 shadow transition-all data-[checked=false]:bg-zinc-50 data-[checked=true]:bg-zinc-950 dark:border-zinc-50 dark:data-[checked=false]:bg-zinc-950 dark:data-[checked=true]:bg-zinc-50"
+              ></button>
+              <p class="text-xs font-medium">Sound</p>
+            </div>
+          </div>
         </div>
-        <div class="flex gap-2">
-          <button
-            aria-label="Sound"
-            data-checked={!sound}
-            onclick={() => (sound = !sound)}
-            class="h-4 w-4 cursor-pointer rounded-sm border border-zinc-950 shadow transition-all data-[checked=false]:bg-zinc-50 data-[checked=true]:bg-zinc-950 dark:border-zinc-50 dark:data-[checked=false]:bg-zinc-950 dark:data-[checked=true]:bg-zinc-50"
-          ></button>
-          <p class="text-xs font-medium">Sound</p>
-        </div>
-      </div>
+      {/if}
     </div>
-  </div>
-</Container>
+  </div></Container
+>
