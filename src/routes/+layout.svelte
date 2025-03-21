cb11efccba70ce9d6c61f319b276857af8d4f868
cz <me@czcs.xyz>
2025-03-20 20:27:18 -0400

    add posthog functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,22 @@
 <script>
   import { i18n } from '$lib/i18n';
   import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+
+  import posthog from 'posthog-js';
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (browser) {
+      posthog.init('phc_jcG75DZ644o41LW6Fk64MAy9MNIDw5AEcgmTSnIKPhW', {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only'
+      });
+    }
+    return;
+  });
+
   import '../app.css';
-
-  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-  import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
-  injectSpeedInsights();
-  injectAnalytics();
 
   let { children } = $props();
 </script>
